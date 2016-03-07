a635a107a0e660ff8d69c5aee5a894687079ae18
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-06 21:27:42 -0600

    [ADDED] paper card element

---
+++ assets/js/application/components/card/main.js
@@ -0,0 +1,44 @@
+App.View.extend({
+  name: 'components/card/main',
+  data_source:[
+    {key: 'heading', required: false},
+    {key: 'img_url', required: false},
+    {key: 'content', required: false},
+    {key: 'content_data', required: false},
+    {key: 'footer', required: false},
+    {key: 'footer_data', required: false},
+  ],
+  init_functions:[
+    'setup',
+    'setupContent',
+    'setupFooter',
+  ],
+
+  setup: function() {
+    this._control = {
+      content_view: false,
+      footer_view: false,
+    }
+
+    this.display = {
+      heading: this.data.heading,
+      img_url: this.data.img_url,
+      content: this.data.content,
+      content_data: this.data.content_data,
+      footer: this.data.footer,
+      footer_data: this.data.footer,
+    };
+  },
+
+  setupContent: function() {
+    if (_.has(App.Views,this.data.content)) {
+      this._control.content_view = true;
+    }
+  },
+
+  setupFooter: function() {
+    if (_.has(App.Views,this.data.footer)) {
+      this._control.footer_view = true;
+    }
+  }
+});
--- assets/js/application/pages/home.js
+++ assets/js/application/pages/home.js
@@ -1,3 +1,18 @@
 App.Page.extend({
   name: "pages/home",
+  init_functions: [
+    'setup',
+  ],
+
+  setup: function() {
+
+    this.components = c = {}
+
+    c.text = {
+      heading: 'Text',
+      img_url: 'images/text.png',
+      content: "Variations of text inputs.",
+      footer: "Button here soon",
+    }
+  },
 });
