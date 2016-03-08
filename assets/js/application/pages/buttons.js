9ddfc2382bff1e7f0415e3aa4d8fb24383f77426
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-08 16:31:15 -0600

    [ADDED] icon button examples

--- assets/js/application/pages/buttons.js
+++ assets/js/application/pages/buttons.js
@@ -6,23 +6,71 @@ App.Page.extend({
 
   setup: function() {
     this.components = c = {}
-    var disabled = new App.Model({class:'monkey_cheese', monkey: "cheese"});
+    var disabled = new App.Model({disabled: true});
     window.attrs = disabled;
 
-    c.one = {
+    c.flat = {
+      text: 'Flat',
+      raised: false,
+    };
+
+    c.flat_primary = {
+      text: 'Flat_Primary',
+      raised: false,
+      button_color: 'primary',
+      text_color: 'white',
+    };
+
+    c.flat_accent = {
+      text: 'Flat_Accent',
+      raised: false,
+      button_color: 'accent',
+    };
+
+    c.button = {
+      text: 'Press',
+      event_handler: function(e) {
+        console.log('button pressed');
+      }
+    };
+
+    c.primary = {
       text: 'Press',
       icon: 'arrow-downward',
       button_color: 'primary',
       text_color: 'white',
+    };
+
+    c.accent = {
+      text: 'Press',
+      button_color: 'accent',
+    };
+
+    c.disabled = {
+      text: 'Press',
       attributes: disabled,
     };
 
     c.icon = {
+      icon: 'close',
+    };
+
+    c.icon_primary = {
       icon: "flight-takeoff",
+      button_color: 'primary',
+      icon_color: 'white',
+    };
+
+    c.icon_accent = {
+      icon: "flight-land",
       button_color: 'accent',
       event_handler: function() {
         console.log('flight takeoff');
       },
+    };
+
+    c.icon_disabled = {
+      icon: "alarm-on",
       attributes: disabled,
     };
   },
