c665eeaeacec8b56721bd1c78561c62874dc0238
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-12 19:42:34 -0600

    [CHANGED] header layout to client view

---
+++ assets/js/application/layouts/main.js
@@ -0,0 +1,24 @@
+App.View.extend({
+  name: 'layouts/main',
+  attributes: {
+    style: 'height: 100%;'
+  },
+  events: {
+    'click #title': '_navigateHome',
+  },
+  data_source: [
+    {key: 'page', required: true},
+  ],
+  init_functions: [
+    'setup',
+  ],
+
+  setup: function() {
+    _.bindAll(this, '_navigateHome');
+  },
+
+  _navigateHome: function() {
+    console.log('navigate home');
+    window.location.pathname = "/home";
+  },
+});
--- assets/js/libs/handlebars_helpers.js
+++ assets/js/libs/handlebars_helpers.js
@@ -12,3 +12,18 @@ Handlebars.registerHelper('view', function(view_name,obj){
 
   return new Handlebars.SafeString(placeholder);
 });
+
+Handlebars.registerHelper('page', function(view_name,obj){
+  if (!App.Pages[view_name]){
+    throw "Could not find a page named: "+view_name
+    return
+  }
+  var parent = obj.data.root;
+  obj.parent = parent;
+  var view = new App.Pages[view_name](obj);
+
+  parent.children[view.cid] = view;
+  var placeholder = view.$el.wrap('<temp>').parent().html();
+
+  return new Handlebars.SafeString(placeholder);
+});
