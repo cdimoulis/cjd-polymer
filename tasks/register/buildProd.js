75e92faf41d2cd09cd3ed5e740e4c1cb78cb35bc
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-23 09:41:49 -0500

    [CHANGED] For debugging live

--- tasks/register/buildProd.js
+++ tasks/register/buildProd.js
@@ -18,11 +18,12 @@
 module.exports = function(grunt) {
   grunt.registerTask('buildProd', [
     'compileAssets',
-    'concat',
-    'uglify',
-    'cssmin',
-    'linkAssetsBuildProd',
-    'clean:build',
-    'copy:build'
+    'linkAssets'
+    // 'concat',
+    // 'uglify',
+    // 'cssmin',
+    // 'linkAssetsBuildProd',
+    // 'clean:build',
+    // 'copy:build'
   ]);
 };
