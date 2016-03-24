68bae1dee22b2a2924284d3bea4622540b5fd2e9
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-24 09:37:37 -0500

    [ADDED] Paper slider

--- assets/js/application/components/general/slider.js
+++ assets/js/application/components/general/slider.js
@@ -1,19 +1,25 @@
 App.Component.extend({
-  name: 'components/genera/slider',
+  name: 'components/general/slider',
   tagName: 'paper-slider',
   dependencies: [
     'paper-slider/paper-slider.html',
   ],
   events: {
     'change': '_onChange',
+    'value-change': '_onValueChange',
+    'immediate-value-change': '_onImmediateChange',
   },
   data_source: [
     {key: 'model', required: true},
     {key: 'attribute', required: true},
-    {key: 'max', required: false, default: 10},
+    {key: 'attributes', required: false},
+    {key: 'max', required: false, default: 100},
     {key: 'min', required: false, default: 0},
     {key: 'step', required: false, default: 1},
     {key: 'pin', required: false, default: false},
+    {key: 'markers', required: false}, // Markers do not seem to work
+    {key: 'snap', required: false, default: false},
+    {key: 'immediate', rquired: false, default: false},
   ],
   init_functions: [
     'setup',
@@ -23,8 +29,43 @@ App.Component.extend({
   setup: function() {
     _.bindAll(this, '_onChange');
     this.data.attributes = this.data.attributes || new App.Model();
+    if (!this.data.attributes.has('class')) {
+      this.data.attributes.set('class', '');
+    }
+    this.data.attributes.set('class', this.data.attributes.get('class')+' cjd-slider')
+    var attrs = {};
 
+    this.el.id = this.data.attributes.get('id') || this.cid+'_slider';
 
+    if (!this.data.attributes.has('id')) {
+      this.data.attributes.set('id', this.el.id)
+    }
+
+    this.el.max = this.data.max;
+    this.el.min = this.data.min;
+    this.el.step = this.data.step;
+
+    if (this.data.pin) {
+      this.el.pin = true;
+    }
+
+    // Markers on on documentation, but don't seem to work yet
+    if (!!this.data.markers && this.data.markers.length != 0) {
+      this.el.markers = '['+this.data.markers.toString()+']';
+      this.el.maxMarkers = this.data.markers.length;
+      // Snap can only work if there are markers
+      if (this.data.snap) {
+        this.el.snap = true;
+      }
+    }
+
+    // Setup listeners for model
+    this.listenTo(this.data.model, 'change:'+this.data.attribute, function(model, value) {
+      this.el.value = value;
+    });
+
+    // Setup listeners for attributes
+    this.listenTo(this.data.attributes, 'change', this.setupAttributesModel);
   },
 
   setupAttributesModel: function() {
@@ -41,6 +82,12 @@ App.Component.extend({
   },
 
   _onChange: function() {
-    console.log('change', this.el.value);
+    this.data.model.set(this.data.attribute, this.el.value);
   },
+
+  _onImmediateChange: function() {
+    if (this.data.immediate) {
+      this.data.model.set(this.data.attribute, this.el.immediateValue);
+    }
+  }
 });
--- assets/js/application/pages/misc.js
+++ assets/js/application/pages/misc.js
@@ -6,10 +6,35 @@ App.Page.extend({
 
   setup: function() {
     var model = new App.Model();
+    var attrs = new App.Model({disabled: true})
     window.model = model;
+    window.attrs = attrs;
     this.components = c = {};
 
-    
+    c.slider = {
+      model: model,
+      attribute: 'slider',
+    };
+
+    c.pin = {
+      model: model,
+      attribute: 'pin',
+      max: 75,
+      min: 25,
+      pin: true,
+    };
+
+    // Markers don't seem to actually work,
+    // though they are in the documentation
+    c.markers = {
+      model: model,
+      attribute: 'marker',
+      attributes: attrs,
+      max: 5,
+      min: 0,
+      markers: [1,2,3,4],
+      snap: true,
+    }
 
   },
 });
