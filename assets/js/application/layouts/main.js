3bacc70d344b30029e86726628356537d622d140
Chris Dimoulis <christopherjamesd@gmail.com>
2016-03-23 19:22:12 -0500

    [CHANGED] only import polymer

--- assets/js/application/layouts/main.js
+++ assets/js/application/layouts/main.js
@@ -1,11 +1,11 @@
 App.View.extend({
   name: 'layouts/main',
-  dependencies: [
-    "paper-drawer-panel/paper-drawer-panel.html",
-    "paper-scroll-header-panel/paper-scroll-header-panel.html",
-    "paper-toolbar/paper-toolbar.html",
-    "paper-icon-button/paper-icon-button.html",
-  ],
+  // dependencies: [
+  //   "paper-drawer-panel/paper-drawer-panel.html",
+  //   "paper-scroll-header-panel/paper-scroll-header-panel.html",
+  //   "paper-toolbar/paper-toolbar.html",
+  //   "paper-icon-button/paper-icon-button.html",
+  // ],
   attributes: {
     style: 'height: 100%;'
   },
